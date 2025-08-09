e84aa1dddbd5e97eaf605239197fd8b8b0b49d49
Tjens23 <tjens23@student.sdu.dk>
2025-08-09 19:54:39 +0200

    fix: included error logging for deleteUser and updateUser

--- user-service/src/services/user.service.ts
+++ user-service/src/services/user.service.ts
@@ -44,7 +44,7 @@ export const updateUser = async (req: Request, res: Response) => {
       user: { id: user.id, email: user.email },
     });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: `Internal server error, ${error}` });
   }
 };
 
@@ -59,6 +59,6 @@ export const deleteUser = async (req: Request, res: Response) => {
     await user.remove();
     return res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: `Internal server error: ${error}` });
   }
 };
