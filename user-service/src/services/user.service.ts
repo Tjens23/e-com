e03521b8247b0b1d6a185cb3011d8e6d8d8418a3
Tjens23 <tjens23@student.sdu.dk>
2025-08-11 11:07:51 +0200

    fix: User can now delete account

--- user-service/src/services/user.service.ts
+++ user-service/src/services/user.service.ts
@@ -75,7 +75,7 @@ export const deleteUser = async (req: Request, res: Response) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    await user.remove();
+    await User.delete({ id: parseInt(id) });
     return res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     return res.status(500).json({ message: `Internal server error: ${error}` });
